7d9995d1863117be97e6e30112ebf4cad0fcbbba
Ghadeer Abd Alhameed <61324743+Gh-abd-alhameed@users.noreply.github.com>
2022-12-03 17:07:20 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,24 @@ const gulp = require("gulp"),
     gulpConcat = require("gulp-concat"),
     webp = require("gulp-webp"),
     babel = require("gulp-babel"),
-    cssPrefix = require("autoprefixer"),
-    minify = require("gulp-minify");
+    cssPrefix = require("gulp-autoprefixer"),
+    minify = require("gulp-minify"),
+cleanCss = require("gulp-clean-css");
 
 gulp.task("boca-css", function () {
-    return gulp.src("project/css/*")
+    return gulp.src("project/css/*.css")
         .pipe(cssPrefix("last 2 versions"))
-        .pipe(gulpConcat("main.css"))
-        .pipe(minify())
+        .pipe(gulpConcat("style.css"))
+        .pipe(cleanCss())
         .pipe(gulp.dest("dist/css"));
 });
 
 gulp.task("boca-js", function () {
-    return gulp.src("project/js/*")
+    return gulp.src("project/js/*.js")
         .pipe(babel({
             presets: ['@babel/env']
         }))
-        .pipe(gulpConcat("main.js"))
+        .pipe(gulpConcat("root.js"))
         .pipe(minify())
         .pipe(gulp.dest("dist/js"));
 });
